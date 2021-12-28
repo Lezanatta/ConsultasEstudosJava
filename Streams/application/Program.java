768a810e34a261351d99d204e333e9e5707a350e
Leandro Zanatta <Leandrozanatta7@gmail.com>
2021-12-28 19:55:19 -0300

    Exercicio pipeline

--- Streams/application/Program.java
+++ Streams/application/Program.java
@@ -21,7 +21,7 @@ public class Program {
 		System.out.println(Arrays.toString(st3.limit(10).toArray()));
 		
 		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, x -> new Long[] 
-				{x[1], x[0] + x[1]}).map(p -> p[0]);
+				{x[1], x[0] + x[1]}).map(x -> x[0]);
 		System.out.println(Arrays.toString(st4.limit(20).toArray()));
 	}
 }
